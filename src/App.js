1bfddb275f5acec06a576a4c55ed9473160c8252
Ari-Jackson <112004676+Ari-Jackson@users.noreply.github.com>
2023-02-17 15:06:16 -0500

    Adds CohortList and StudentList to App with layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import Header from "./componets/Header";
+import Header from "./componets/Header/Header";
+import CohortList from "./componets/CohortList/CohortList";
+import StudentList from "./componets/StudentList/StudentList";
+import { useState } from "react";
 
 function App() {
+  const [studentList, setStudentList] = useState();
   return (
     <>
       <Header />
+      <main>
+        <CohortList />
+        <StudentList />
+      </main>
     </>
   );
 }
