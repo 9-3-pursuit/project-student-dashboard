f2393d43547438ebdfe5d9a27f371ad2c1f198e4
Ari-Jackson <112004676+Ari-Jackson@users.noreply.github.com>
2023-02-18 12:46:03 -0500

    Sorts Cohort List

--- src/componets/CohortList/CohortList.jsx
+++ src/componets/CohortList/CohortList.jsx
@@ -3,8 +3,9 @@ import { studentData } from "../../data/data";
 import SingleCohort from "./SingleCohort";
 
 export default function CohortList({ handleClick }) {
-  const cohortCodes = studentData.reduce(getUniqueCohortCodes, []);
-  const AllCohortOptions = ["All Students", ...cohortCodes];
+  const uniqueCohortCodes = studentData.reduce(getUniqueCohortCodes, []),
+    sortedCohortCodes = sortsCohortCodes(uniqueCohortCodes),
+    AllCohortOptions = ["All Students", ...sortedCohortCodes];
 
   return (
     <div className="cohort-list">
@@ -20,9 +21,25 @@ export default function CohortList({ handleClick }) {
   );
 }
 
-function getUniqueCohortCodes(uniqueCodesArray, { cohort: { cohortCode } }) {
-  if (!uniqueCodesArray.includes(cohortCode)) {
-    uniqueCodesArray = [...uniqueCodesArray, cohortCode];
+function getUniqueCohortCodes(uniqueCodes, { cohort: { cohortCode } }) {
+  if (!uniqueCodes.includes(cohortCode)) {
+    uniqueCodes = [...uniqueCodes, cohortCode];
   }
-  return uniqueCodesArray;
+  return uniqueCodes;
+}
+
+function sortsCohortCodes(cohorts) {
+  const seasons = { Spring: 1, Summer: 2, Fall: 3, Winter: 4 };
+
+  const humanReadableList = cohorts.map(
+    (cohort) => `${cohort.slice(0, -4)} ${cohort.slice(-4)}`
+  );
+
+  const SortedList = humanReadableList.sort((a, b) => {
+    const splitA = a.split(" ");
+    const splitB = b.split(" ");
+    return splitA[1] - splitB[1] || seasons[splitA[0]] - seasons[splitB[0]];
+  });
+
+  return SortedList.map((cohort) => cohort.split(" ").join(""));
 }
