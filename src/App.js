5578da4865e71cb4dd73025be9c8f96c34a4e5c3
joshuanelsondev <joshuanelsondev@gmail.com>
2023-02-18 18:38:56 -0500

    Write logic to sort the seasons

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import React from "react";
+import CohortList from "./components/CohortList";
+import studentData from "./data/data.json"
 
 function App() {
+
   return (
     <div>
       <header>Student Dashboard</header>
+      <main>
+        <CohortList studentData={studentData} />
+        
+      </main>
     </div>
   );
 }
