7fa468b6a08a84b2d0efc0dea4786e13f34ee179
joshuanelsondev <joshuanelsondev@gmail.com>
2023-02-19 12:17:53 -0500

    Update Cohortlist

--- src/components/CohortList.js
+++ src/components/CohortList.js
@@ -1,34 +1,15 @@
+import Cohorts from "./Cohorts";
 
 export default function CohortList({ studentData }) {
 
-    const cohortSet = new Set();
-    studentData.map(student => cohortSet.add(`${student.cohort.cohortCode} ${student.cohort.cohortStartDate}`));
-    const cohortArray = Array.from(cohortSet);
-    const seasonAndDateArray = cohortArray.map(date => date.split(" "))
-    const sortedSeasonAndDateArray = seasonAndDateArray.sort((numericDateA, numericDateB) => new Date(numericDateB[1]) - new Date(numericDateA[1]))
-    const sortedSeasonsArray = sortedSeasonAndDateArray.reduce((newSeasonArr, seasonAndDateArr) => {
-        if (!newSeasonArr.includes(seasonAndDateArr[0])) {
-            newSeasonArr.push(seasonAndDateArr[0])
-        }
-        return newSeasonArr
-    }, [])
-    const spacedSeasonsArray = sortedSeasonsArray.map(season => {
-        const spacedSeason = season.split('')
-        return spacedSeason
-    })
-    const sortedAndSpacedSeasons = spacedSeasonsArray.map(spacedSeasons => {
-        spacedSeasons.splice(-4, 0, " ")
-        const formattedSeason = spacedSeasons.join("")
-        return formattedSeason
-    })
     
-    console.log(sortedAndSpacedSeasons)
     
     return(
         <div className="cohortDiv">
             <h1 className="cohortHeader">Choose a Class by Start Date</h1>
             <div className="cohortDate">
-                
+                <h4>All Students</h4>
+                <Cohorts studentData={studentData} />
             </div>
 
         </div>
---
+++ src/components/Cohorts.js
@@ -0,0 +1,31 @@
+
+export default function Cohorts({ studentData}) {
+    
+    const cohortSet = new Set();
+    studentData.map(student => cohortSet.add(`${student.cohort.cohortCode} ${student.cohort.cohortStartDate}`));
+    const unsortedCohortArray = Array.from(cohortSet);
+    const seasonAndDateArray = unsortedCohortArray.map(date => date.split(" "))
+    const sortedSeasonAndDateArray = seasonAndDateArray.sort((numericDateA, numericDateB) => new Date(numericDateB[1]) - new Date(numericDateA[1]))
+    const sortedCohortsArray = sortedSeasonAndDateArray.reduce((newSeasonArr, seasonAndDateArr) => {
+        if (!newSeasonArr.includes(seasonAndDateArr[0])) {
+            newSeasonArr.push(seasonAndDateArr[0])
+        }
+        return newSeasonArr
+    }, [])
+    const spacedLettersCohortsArray = sortedCohortsArray.map(cohort => {
+        const spacedLetters = cohort.split('')
+        return spacedLetters
+    })
+    const sortedAndFormattedCohorts = spacedLettersCohortsArray.map(spacedLettersArray => {
+        spacedLettersArray.splice(-4, 0, " ")
+        const formattedSeason = spacedLettersArray.join("")
+        return formattedSeason
+    })
+    
+    return sortedAndFormattedCohorts.map(cohorts => {
+        return(
+            <h4 key={cohorts}>{cohorts}</h4>
+        )
+    })
+
+}
