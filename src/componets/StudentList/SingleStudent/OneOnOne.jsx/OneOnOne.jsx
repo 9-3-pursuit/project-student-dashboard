ba34b512bb31eff4c7a53287db0fae25d23bb55a
Ari-Jackson <112004676+Ari-Jackson@users.noreply.github.com>
2023-02-19 09:25:48 -0500

    Adds state and style to One on One

--- src/componets/StudentList/SingleStudent/OneOnOne.jsx/OneOnOne.jsx
+++ src/componets/StudentList/SingleStudent/OneOnOne.jsx/OneOnOne.jsx
@@ -1,25 +1,43 @@
 import "./OneOnOne.css";
+import { useState } from "react";
+
 export default function OneOnOne({ notes }) {
-  function handleClick(e) {
+  const initialState = { commentor: "", comment: "" };
+  const [form, setForm] = useState(initialState);
+
+  function handleSubmit(e) {
     e.preventDefault();
+    alert(`${form.commentor} says "${form.comment}"`);
+    setForm({ ...initialState });
   }
+
+  function handleChange(e) {
+    setForm({ ...form, [e.target.id]: e.target.value });
+  }
+  console.log(form.commentor);
   return (
     <div className="one-on-one">
-      <form>
+      <form onSubmit={handleSubmit}>
         <h3>1-on-1</h3>
-        <label htmlFor="commentor">
-          Commentor Name
-          <input type={"text"} id={"commentor"} />
-        </label>
-        <label htmlFor="comment">
-          Comment
-          <input type={"text"} id={"comment"} />
-        </label>
-        <input type={"submit"} value="Add Comment" onClick={handleClick} />
+        <label htmlFor="commentor">Commentor Name</label>
+        <input
+          id={"commentor"}
+          type={"text"}
+          value={form.commentor}
+          onChange={handleChange}
+        />
+        <label htmlFor="comment">Comment</label>
+        <textarea
+          id={"comment"}
+          row={"10"}
+          value={form.comment}
+          onChange={handleChange}
+        />
+        <input type={"submit"} value={"Add Comment"} />
       </form>
       <ul>
         {notes.map(({ commenter, comment }) => (
-          <li>
+          <li key={comment}>
             {commenter} says "{comment}"
           </li>
         ))}
