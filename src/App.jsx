22477cc796be841eaf6ec2a0b4da56049d11a12d
HaroldF415 <harold.f415@gmail.com>
2023-02-19 23:50:50 -0500

    addNewNoteToStudent() completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,22 @@ import { React, useState } from "react";
 import studentData from "./data/data.json";
 import Cohorts from "./components/Cohorts";
 import Header from "./components/Header";
+import Students from "./components/Students";
 
 import "./styles/App.css";
 
 const App = () => {
+  const [studentDataClone, setStudentDataClone] = useState(studentData);
+
+  const addNewNoteToStudent = (studentID, newNote) => {
+    const clonedStudentDataForNoteUpdate = studentData;
+    const studentFoundInClonedStudentData = clonedStudentDataForNoteUpdate.find((student) => studentID === student.id);
+    studentFoundInClonedStudentData.notes.push(newNote);
+    setStudentDataClone(clonedStudentDataForNoteUpdate);
+  };
+
+  const handleFilterStudentsByCohortClick = (cohort) => {};
+
   return (
     <div>
       <h1>Student Dashboard</h1>
