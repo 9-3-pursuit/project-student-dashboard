1d8bff92e3c1c1756bb5281092c7e6e245976375
HaroldF415 <harold.f415@gmail.com>
2023-02-20 17:58:51 -0500

    added dark/light mode button toggle

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,17 @@
 import "../styles/Header.css";
+import { useState } from "react";
 
 const Header = () => {
+  const [mode, setMode] = useState(false);
+
+  const toggleDarkAndLightMode = () => {
+    setMode(!mode);
+  };
+
   return (
     <header>
       <h1>Student Dashboard</h1>
+      <button onClick={toggleDarkAndLightMode}>{mode ? "Dark" : "Light"}</button>
     </header>
   );
 };
