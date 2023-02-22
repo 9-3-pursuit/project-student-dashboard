ae066e12900cf71aa06dc1f3574780510714016e
joshuanelsondev <joshuanelsondev@gmail.com>
2023-02-22 17:57:51 -0500

    Create functionality for the One-on-One notes

--- src/components/OneOnOneform.js
+++ src/components/OneOnOneform.js
@@ -1,14 +1,20 @@
 import { useState } from "react";
 
-export default function OneOnOneForm() {
-    const [note, setNote] = useState({
+export default function OneOnOneForm({ student }) {
+    const blankNote = {
         commenter: "",
         comment: ""
-    })
+    }
+    const [note, setNote] = useState(blankNote)
+
+    const [notesArray, setNotesArray] = useState([])
 
     function handleAddNote(event) {
         event.preventDefault();
+        setNotesArray([...notesArray, note ]);
+        setNote(blankNote)
     }
+    
 
     function handleNoteInput(event) {
         setNote({...note, [event.target.id]: event.target.value})
@@ -16,15 +22,19 @@ export default function OneOnOneForm() {
     return(
         <div>
             <h4>1-on-1 Notes</h4>
-            <form>
+            <form onSubmit={handleAddNote}>
                 <label htmlFor="commenter">Commenter Name</label>
-                <input type="text" id="commenter" value={note.commenter} onChange={handleNoteInput} />
+                <input type="text" id="commenter" value={note.commenter} onChange={handleNoteInput} required />
                 <label htmlFor="comment">Comment</label>
-                <input type="text" id="comment" value={note.comment} onChange={handleNoteInput} />
-                <button onClick={handleAddNote}>Add Note</button>
+                <input type="text" id="comment" value={note.comment} onChange={handleNoteInput} required />
+                <input type="submit" value={"Add Note"} />
             </form>
             <ul>
-
+                {notesArray.map((noteObj, i) => {
+                    return (
+                        <li key={`${noteObj.commenter}${i}`}> <span>{noteObj.commenter}</span> says, "{noteObj.comment}"</li>
+                    )
+                })}
             </ul>
         </div>
     )
