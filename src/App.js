201da4c4ca5aa4986ac1f18e0a7814c75d0dd81e
Markeya Mckoy-Carree <mcmcarree@gmail.com>
2023-02-21 19:48:32 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-
+import "./App.css";
 function App() {
   return (
-    <div>
-      <h1>Student Dashboard</h1>
-    </div>
-  );
-}
+<div className="all">
+<div className="header">
+  <h1>Student Dashboard</h1>
+</div>
+<div className="classCohort">
+  <h2>Choose a Class by Start Date</h2>
+  <li>All Students</li>
+  <hr></hr>
+  <li>Winter 2026</li>
+  <hr></hr>
+  <li>Fall 2026</li>
+  <hr></hr>
+  <li>Summer 2026</li>
+  <hr></hr>
+  <li>Spring 2026</li>
+  <hr></hr>
+  <li>Winter 2025</li>
+  <hr></hr>
+  <li>Fall 2025</li>
+  <hr></hr>
+  <li>Summer 2025</li>
+  <hr></hr>
+  <li>Spring 2025</li>
+</div>
+</div>
+);
+  }
 
 export default App;
