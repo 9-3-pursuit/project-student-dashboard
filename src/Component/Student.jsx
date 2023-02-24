3ec465c23e7276d51e357a81fbe7fd3e88ddf24a
Diandre Miller <millerdiandre@gmail.com>
2023-02-24 00:09:43 -0500

    on track to graduate added

--- src/Component/Student.jsx
+++ src/Component/Student.jsx
@@ -31,6 +31,33 @@ const Student = ({ student }) => {
 
   const onTrackToGraduateMessage = () => onTrackToGraduate() ? 'On Track to Graduate' :
     'Not on Track to Graduate';
+  
+  const onTrackToGraduateColor = () => onTrackToGraduate() ? 'green' : 'red';
+
+  const codeWarsCurrentComparedToGoalColor = () => {
+    const codewarsCurrentTotal = student.codewars.current.total;
+    const codewarsGoalTotal = student.codewars.goal.total;
+    const percentageToGoal = codewarsCurrentTotal / codewarsGoalTotal;
+    if (percentageToGoal >= 1) {
+      return 'green';
+    } else if (percentageToGoal >= 0.5) {
+      return 'yellow';
+    } else {
+      return 'red';
+    }
+  }
+
+  const codeWarsCurrentComparedToGoalPercentage = () => { 
+    const codewarsCurrentTotal = student.codewars.current.total;
+    const codewarsGoalTotal = student.codewars.goal.total;
+    const percentageToGoal = ((codewarsCurrentTotal / codewarsGoalTotal)*100).toFixed(0);
+    return percentageToGoal;
+
+  }
+
+  console.log("codeWarsCurrentComparedToGoalPercentage", codeWarsCurrentComparedToGoalPercentage())
+
+  
 
 
 
@@ -42,23 +69,29 @@ const Student = ({ student }) => {
   return (
     <div key={student.id} className='card1'>
       <div>
-        <p className='onTrackToGraduate' >{onTrackToGraduateMessage()}</p>
+      <p className='onTrackToGraduate' style={{ color: onTrackToGraduateColor() }}>
+    {onTrackToGraduateMessage()}
+        </p>
+        <h4>Codewars:</h4>
       <p>Codewars Current Total: {student.codewars.current.total}</p>
-      <p>Codewars Goal Total: {student.codewars.goal.total}</p>
-      <p>Certifications:</p>
+        <p>Codewars Goal Total: {student.codewars.goal.total}</p>
+        <p style={{ color: codeWarsCurrentComparedToGoalColor() }}>Codewars Current Compared to Goal:
+         {codeWarsCurrentComparedToGoalPercentage()}%
+        </p>
+      <h4>Certifications:</h4>
       <ul>
         <li>Resume: {student.certifications.resume ? '✅' : '❌'}</li>
         <li>LinkedIn: {student.certifications.linkedin ? '✅' : '❌'}</li>
         <li>Github: {student.certifications.github ? '✅' : '❌'}</li>
         <li>Mock Interview: {student.certifications.mockInterview ? '✅' : '❌'}</li>
       </ul>
-      <p>Notes:</p>
+      <h4>Notes:</h4>
       <ul>
         {student.notes.map((note) => (
           <li key={note.id}> {note.commenter} says, "{note.comment}"</li>
         ))}
       </ul>
-      <p>Cohort:</p>
+      <h4>Cohort:</h4>
       <ul>
         <li>Cohort Start Date: {student.cohort.cohortStartDate}</li>
         <li>Scores:</li>
