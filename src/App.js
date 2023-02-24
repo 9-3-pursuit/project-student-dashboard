b119043436560e070fa702bfcd24b119803bcbc1
Shaniqua Coston <shaniquacoston@pursuit.org>
2023-02-24 06:43:49 -0500

    Students list almost done.

--- src/App.js
+++ src/App.js
@@ -1,54 +1,70 @@
 import React from "react";
 import { useState } from "react";
-import "./index.css"
+import "./index.css";
 import studentsList from "./data/data.json";
 import Students from "./components/Students";
 import Cohorts from "./components/Cohorts";
-import "./helpers/cohorts"
 
-const students = studentsList.studentsList
 // console.log(students)
-function StudentRoster({students}) {
-  students.map((student)=> {
-  return (
-    <div className="students" key={student.id}>
-      <ul >
-      <h2>All Students</h2>
-      <p>Total Students: {students.length}</p>
-      </ul>
-    </div>
-  )
+function StudentRoster({ studentsList }) {
+  studentsList.map((student) => {
+    return (
+      <div className="students" key={student.id}>
+        <ul>
+          <h2>All Students</h2>
+          <p>Total Students: {studentsList.length}</p>
+        </ul>
+      </div>
+    );
+  });
 }
-)}
+
+function cohortList() {
+  studentsList.map((student) => {
+
+    return (
+      <section className="cohortList">
+        
+        <ul className="sortedCohorts">
+          <li>All Students</li>
+        </ul>
+      </section>
+    );
+  })
+}
 
 function App() {
-  const [cohorts, getCohorts] = useState([]);
-  // console.log(cohorts)
+  const [userInput, setUserInput] = (studentsList);
+  const [activeCohort, setActiveCohort]= useState(studentsList);
+  console.log(activeCohort)
 
-  function getCohortList(cohortToAdd){
-   return (
-    <div className="cohorts">
-      <h4>Choose a Class by Start Date</h4>
-    </div>
-   )
-    // getCohorts([...cohorts, cohortToAdd]);
-  }
-  getCohortList()
-
-  
-  
-  
-  
   return (
-    <div>
+    <div className="App">
       <header className="header">
-        <h1>Student Dashboard</h1>
+        <h1 className="color-change-5x">Student Dashboard</h1>
       </header>
-      <Cohorts getCohortList={getCohortList} students={students}/>
-      <Students students={students} />
+      
+
+      <section className="cohortDiv">
+      <h2>Choose a Class by Start Date</h2>
+      <br></br>
+      <Cohorts students={studentsList} setActiveCohort={setActiveCohort} activeCohort={activeCohort}/>
+      </section>
+
+      <div className="studentDiv">
+      <h2 className="studentsHeading">All Students</h2>
+      <br></br>
+      <p className="studentsTotal">Total Students: {studentsList.length} </p>
+      <br></br>
+      < Students students={studentsList} />
+
+      </div>
+     
+     
+
+      
     </div>
   );
 }
 
 export default App;
-
--- src/components/Cohorts.js
+++ src/components/Cohorts.js
@@ -1,14 +1,83 @@
-export default function Cohorts({students}) {
+import { getFID } from "web-vitals";
+import { useState } from "react";
+import studentsList from "../data/data.json";
+
+
+
+function filteredCohortLists({activeCohort}){
+ const filterList = studentsList.filter((student) => {
+    if(student.cohort.cohortCode === activeCohort.cohortId){
+        console.log(activeCohort.chortId)
+    }
+ })
+}
+
+export default function Cohorts({students, setActiveCohort}) {
+
+
+
+  const uniqueCohorts = new Set();
+  const cohortList = students.map((student) => student.cohort.cohortCode);
+
+  for (let cohortId of cohortList) {
+    uniqueCohorts.add(cohortId);
+    console.log(cohortId)
+  }
+  const cohorts = Array.from(uniqueCohorts);
+ 
+
+  const cohortObj = cohorts.map((cohortId) => {
+    const studentsInThisCohort = studentsList.find(
+      (student) => student.cohort.cohortCode === cohortId
+    );
    
-       
-        
-        
+    
+    return {
+      id: studentsInThisCohort.cohort.cohortCode,
+      startDate: new Date(studentsInThisCohort.cohort.cohortStartDate),
+      
+    };
+  });
+
+  const sortedList = cohortObj.sort((a, b) => a.startDate - b.startDate);
+  console.log(sortedList)
+
+  const formattedTitles = sortedList.map((cohort) => {
+    const cohortMonth = cohort.id.slice(0, cohort.id.length - 4);
+    const cohortYear = cohort.id.slice(cohort.id.length - 4);
+    const displayFormat = `${cohortMonth} ${cohortYear}`;
+    
     return (
-        <section>
-            <h1>Choose a Class by Start Date</h1>
-            <ul>Today
-                <li></li>
-            </ul>
-        </section>
+      <div className="cohortList">
+        <ul>
+          
+          <li  className="cohorts" key={students.id} onClick={()=> {
+            setActiveCohort(studentsList)
+          }}>
+            {displayFormat}
+            
+          </li>
+        </ul>
+      </div>
+    );
+  });
+
+
+  const cohortTitles = formattedTitles.map((title) => {
+ 
+    return (
+        <>
+        
+        {title}
+        </>
     )
- }  
+    });
+  return (
+    < >
+     <li className="allStudents">All Students</li>
+     {cohortTitles}   
+    </>
+  )
+}
+        
+        
