08ff0837c550cea41876e8088736cc5ae59a8ea1
Tafari Excell <excell.tafari@gmail.com>
2023-02-27 08:19:34 -0500

    Incomplete project

--- src/App.js
+++ src/App.js
@@ -1,19 +1,48 @@
 import React from "react";
 import { useState } from "react";
 import StudentList from "./StudentList";
+import CohortList from "./CohortList";
+import data from "./data/data.json";
 
-import data from "./data/data.json"
+// import "./index.css"
+
 function App() {
-  // const studentsData = data
-  // const [addStudent, setAddStudent] = useState([])
+  const [allStudents, setAllStudents] = useState(250);
 
-  // console.log("students:", studentsData)
+  //or 250(being the current state when page is initially loaded and when All students is clicked on)
+  // if (allStudents) {
+  //   return true;
+
+  // }else {
+  //   return false;
+  // }
+  const [activeCohort, setActiveCohort] = useState("Winter2025");
+  console.log(activeCohort);
+
+  const cohortFilteredArray = data.filter((student) => {
+    if (student.cohort.cohortCode === activeCohort) {
+      console.log("active", activeCohort);
+      return true;
+    }
+    if (!student.cohort.cohortCode) {
+      console.log("data", data.length);
+      return data.length;
+    }
+  });
+
   return (
     <div>
-      <h1>Student Dashboard</h1>
+      <h1 className="apptitle">Student Dashboard</h1>
+
       <section className="students">
-        <StudentList data = {data} />
-      {/* <StudentList data={data} addStudent={addStudent} setAddStudent={setAddStudent} /> */}
+        <StudentList
+          data={cohortFilteredArray}
+          setAllStudents={setAllStudents}
+        />
+        <section className="cohorts">
+          <CohortList data={data} setActiveCohort={setActiveCohort} />
+          {/* <StudentList data={data} addStudent={addStudent} setAddStudent={setAddStudent} /> */}
+        </section>
       </section>
     </div>
   );
