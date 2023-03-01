81458d96d475e569ecdb4f0889640fcc174b7d2b
shaquala <96317236+shaqfred@users.noreply.github.com>
2023-03-01 13:36:00 -0500

    updated code in cohortList component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import { useState } from "react";
+// import { useState } from "react";
 import CohortDescription from "./Components/CohortDescription";
 import CohortList from "./Components/CohortList";
 import StudentCard from "./Components/StudentCard";
 import React from "react";
 
 function App() {
-  const [comment, setComment] = useState("");
+  // const [comment, setComment] = useState("");
 
-  const handleSubmit = (e) => e.preventDefault();
+  // const handleSubmit = (e) => e.preventDefault();
   // alert("New note by from ${commenter}: ${comment}");
-  setComment("");
-  setComment("");
+  // setComment("");
+  // setComment("");
 
-  const handleCommenter = (e) => {
-    setComment(e.target.value);
-  };
-  const handleComment = (e) => {
-    setComment(e.target.value);
-  };
+  // const handleCommenter = (e) => {
+  //   setComment(e.target.value);
+  // };
+  // const handleComment = (e) => {
+  //   setComment(e.target.value);
+  // };
 
   return (
     <div>
--- src/Components/CohortList.js
+++ src/Components/CohortList.js
@@ -2,44 +2,68 @@ import students from "../data/data.json";
 import React from "react";
 
 function CohortList() {
-  <h2>CohortList</h2>;
-  return;
+  const finalCohortList = {};
+  // console.log(students);
+  students.forEach((student) => {
+    if (finalCohortList[student.cohort.cohortCode]) {
+      finalCohortList[student.cohort.cohortCode].push(student);
+    } else {
+      finalCohortList[student.cohort.cohortCode] = [student];
+    }
+  });
+  return <h2>Choose a Class by Start Date</h2>;
 }
 
 const seasons = ["Winter", "Spring", "Summer", "Fall"];
 
 function getAllCohortsList(students) {
-  //   const cohorts = new Set();
-  console.log(students);
+  //   const cohort = new Set();
+  //   console.log(students);
 }
+
+// cohorts.add("hello");
+// cohorts.add("hello");
+
+// console.log(cohorts);
 
 getAllCohortsList(students);
 
-const allCohorts = students.map((student) => student.cohort.cohortCode);
-
 // console.log(students);
 
-console.log(allCohorts);
+// console.log(allCohorts);
 
-//   for (let cohort of allCohorts) {
-//     cohorts.add(cohort);
+// console.log(cohorts);
 
-const cohortIDs = Array.from(allCohorts);
-console.log(cohortIDs);
+// console.log(allCohorts);
 
-const finalCohortList = cohortIDs.map((cohortId) => {
-  const studentBelongingToCohort = students.find(
-    (student) => student.cohort.cohortCode === cohortId
-  );
-  return {
-    id: studentBelongingToCohort.cohort.cohortCode,
-    startDate: new Date(studentBelongingToCohort.cohort.cohortStartDate),
-    displayName: formatCohortName(studentBelongingToCohort.cohort.cohortCode),
-  };
-});
-console.log(finalCohortList);
+// console.log(cohortIDs);
 
-function formatCohortName(cohortId) {
-  console.log(cohortId);
+// console.log(finalCohortList);
+// const finalCohortList = cohortIDs.map((cohortId) => {
+//   const studentBelongingToCohort = students.find(
+//     (student) => student.cohort.cohortCode === cohortId
+//   );
+//   return {
+//     id: studentBelongingToCohort.cohort.cohortCode,
+//     startDate: new Date(studentBelongingToCohort.cohort.cohortStartDate),
+//     displayName: formatCohortName(studentBelongingToCohort.cohort.cohortCode),
+//   };
+// });
+
+// console.log(finalCohortList);
+
+function formatCohortName() {
+  const cohorts = new Set();
+  const allCohorts = students.map((student) => student.cohort.cohortCode);
+  for (let cohort of allCohorts) {
+    cohorts.add(cohort);
+  }
+  const cohortIDs = Array.from(cohorts);
+  console.log(cohortIDs);
+  const formatCohortIds = cohortIDs.map((cohortId) => {
+    return 
+  });
+  //   console.log(cohortId);
 }
+formatCohortName()
 export default CohortList;
