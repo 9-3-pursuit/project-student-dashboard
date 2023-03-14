c56b7259b4207e4315e8a58747696353d7b3b35a
ryanfodera <ryanfodera@pursuit.org>
2023-03-14 12:50:46 -0400

    fixed issue, should run now

--- src/components/StudentCard.jsx
+++ src/components/StudentCard.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import userImg from "../assets/user-img3.png";
+import userImg from "../assets/user-img.png";
 
 function StudentCard({ student, comments, setComments }) {
   const [showMore, setShowMore] = useState(false);
